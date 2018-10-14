9e420d3c7cfc8502846672e633829420800f6273
jbyrneie <jbyrne@glgroup.com>
2018-10-14 21:42:18 +0100

    Vendors page

---
+++ src/pages/VendorSurveysPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { observer, inject } from 'mobx-react'
+import AppBar from '../components/AppBar';
+import VendorSurveysByCategory from '../components/VendorSurveysByCategory';
+import VendorSurveysBySurvey from '../components/VendorSurveysBySurvey';
+
+class VendorSurveysPage extends Component {
+  render() {
+    return (
+      <div>
+        <AppBar title='Vendor Surveys' newSurvey={true}/>
+        <VendorSurveysByCategory />
+        <VendorSurveysBySurvey />
+      </div>
+    );
+  }
+}
+export default inject((allStores) => ({ ...allStores }))(observer(VendorSurveysPage))
